1f8af592c43af66f0163769457f725564e30fb26
sakiohma <sakiohma@github.com>
2018-09-25 04:19:47 -0400

    Script changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,36 +23,3 @@ $(document).ready(function () {
         alert("Rocky hates water!");
     });
 });
-
-/*
-$(document).ready(function () {
-    $("#pic2").hover(function () {
-        alert("Chase is allergic to cats!");
-    });
-});
-
-$(document).ready(function () {
-    $("#pic3").hover(function () {
-        alert("Rubble loves food!");
-    });
-});
-
-$(document).ready(function () {
-    $("#pic4").hover(function () {
-        alert("Skye is very good at flying!");
-    });
-});
-
-$(document).ready(function () {
-    $("#pic5").hover(function () {
-        alert("Zuma was once a mer-dog!");
-    });
-});
-
-$(document).ready(function () {
-    $("#pic6").hover(function () {
-        alert("Rocky hates water!");
-    });
-});
-
-*/

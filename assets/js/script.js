c2fb52c436d0124f215861f094bff3ccaa1fcd35
sakiohma <sakiohma@github.com>
2018-09-25 02:31:31 -0400

    Js file edits

---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+    $("#pic1").click(function () {
+        alert("Marshall is very clumsy!");
+    });
+
+    $("#pic2").click(function () {
+        alert("Chase is allergic to cats!");
+    });
+   
+    $("#pic3").click(function () {
+        alert("Rubble loves food!");
+    });
+
+    $("#pic4").click(function () {
+        alert("Skye is very good at flying!");
+    });
+
+    $("#pic5").click(function () {
+        alert("Zuma was once a mer-dog!");
+    });
+
+    $("#pic6").click(function () {
+        alert("Rocky hates water!");
+    });
+});
+
+/*
+$(document).ready(function () {
+    $("#pic2").hover(function () {
+        alert("Chase is allergic to cats!");
+    });
+});
+
+$(document).ready(function () {
+    $("#pic3").hover(function () {
+        alert("Rubble loves food!");
+    });
+});
+
+$(document).ready(function () {
+    $("#pic4").hover(function () {
+        alert("Skye is very good at flying!");
+    });
+});
+
+$(document).ready(function () {
+    $("#pic5").hover(function () {
+        alert("Zuma was once a mer-dog!");
+    });
+});
+
+$(document).ready(function () {
+    $("#pic6").hover(function () {
+        alert("Rocky hates water!");
+    });
+});
+
+*/
